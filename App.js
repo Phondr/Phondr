da6e6a60c1a587b33bacbea2faf8a9f1e59bd123
Jian Cong Ma <jiancongma@protonmail.com>
2019-11-21 13:54:10 -0600

    created the component files

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import { Platform, StatusBar, StyleSheet, View } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
 
 import AppNavigator from './navigation/AppNavigator'
+import AnatomyExample from './components/hellowworld'
 
 export default function App(props) {
   const [isLoadingComplete, setLoadingComplete] = useState(false)
@@ -24,6 +25,7 @@ export default function App(props) {
       <View style={styles.container}>
         {Platform.OS === 'ios' && <StatusBar barStyle='default' />}
         <AppNavigator />
+        {/* <AnatomyExample /> */}
       </View>
     )
   }
