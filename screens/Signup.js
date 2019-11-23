5da7c0adffe136cfe1bdb80c4efed486b049bc14
Eric Grossman <eric.josh.grossman@gmail.com>
2019-11-23 13:14:08 -0600

    compose with redux

--- screens/Signup.js
+++ screens/Signup.js
@@ -1,57 +1,56 @@
-import React, { Component } from "react";
-import { Text, View, StyleSheet, Button } from "react-native";
-import { connect } from "react-redux";
-import { userSignUp } from "../client/redux/user";
+import React, { Component } from 'react'
+import { Text, View, StyleSheet, Button } from 'react-native'
+import { connect } from 'react-redux'
+import { userSignUp } from '../redux/user'
 
-
-import t from "tcomb-form-native";
+import t from 'tcomb-form-native'
 
 const User = t.struct({
   fullname: t.String,
   age: t.Integer,
   // gender: t.String,
   email: t.String,
-  password: t.String
-});
+  password: t.String,
+})
 
 const options = {
   fields: {
     fullname: {
-      error: "You need your fullname to sign up"
+      error: 'You need your fullname to sign up',
     },
     age: {
-      error: "You need a valid age to sign up"
+      error: 'You need a valid age to sign up',
     },
     // gender: {
     //   error: "You need a valid gender to sign up"
     // },
     email: {
-      error: "You need a valid email to sign up"
+      error: 'You need a valid email to sign up',
     },
     password: {
       password: true,
       secureTextEntry: true,
-      error: "You need a valid password to sign up"
-    }
-  }
-};
+      error: 'You need a valid password to sign up',
+    },
+  },
+}
 
-const Form = t.form.Form;
+const Form = t.form.Form
 
 export class Signup extends Component {
   constructor() {
-    super();
+    super()
 
-    this.signup = this.signup.bind(this);
+    this.signup = this.signup.bind(this)
   }
 
   async signup() {
-    const values = this._form.getValue();
+    const values = this._form.getValue()
     try {
-      await this.props.addUser(values);
+      await this.props.addUser(values)
     } catch (error) {
-      alert("COULD NOT SIGNUP");
-      console.log(error);
+      alert('COULD NOT SIGNUP')
+      console.log(error)
     }
   }
 
@@ -65,10 +64,10 @@ export class Signup extends Component {
             options={options}
             style={styles.formcontainer}
           />
-          <Button title="Submit" onPress={this.signup} />
+          <Button title='Submit' onPress={this.signup} />
         </View>
       </View>
-    );
+    )
   }
 }
 
@@ -77,13 +76,13 @@ export const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 35,
     padding: 10,
-    backgroundColor: "#F5FCFF"
+    backgroundColor: '#F5FCFF',
   },
   phonderimage: {
     width: 200,
     height: 200,
-    position: "relative",
-    justifyContent: "center"
+    position: 'relative',
+    justifyContent: 'center',
   },
   textInput: {
     //borderBottomColor: "#CCCCCC",
@@ -99,21 +98,21 @@ export const styles = StyleSheet.create({
   },
   logintext: {
     margin: 2,
-    fontSize: 30
+    fontSize: 30,
   },
   formcontainer: {
-    justifyContent: "center",
-    width: "100%",
-    backgroundColor: "#ffffff"
-  }
-});
+    justifyContent: 'center',
+    width: '100%',
+    backgroundColor: '#ffffff',
+  },
+})
 
 const mapStateToProps = state => ({
-  user: state.user
-});
+  user: state.user,
+})
 
 const mapDispatchToProps = dispatch => ({
-  addUser: values => dispatch(userSignUp(values))
-});
+  addUser: values => dispatch(userSignUp(values)),
+})
 
-export default connect(mapStateToProps, mapDispatchToProps)(Signup);
+export default connect(mapStateToProps, mapDispatchToProps)(Signup)
