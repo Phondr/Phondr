e165c4fe7b360c9c5340bf8ad49c047fa6b42d30
Jian Cong Ma <jiancongma@protonmail.com>
2019-11-26 11:22:10 -0600

    doing type options

--- components/MapView.js
+++ components/MapView.js
@@ -1,20 +1,16 @@
 import React, {Component} from 'react'
 import {
-  Text,
   View,
-  Form,
   Picker,
   Item,
   Icon,
-  Content,
-  Container,
-  Button
 } from 'native-base'
 import {StyleSheet} from 'react-native'
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps'
 import googlePlaceApiKey from '../secrets'
 import axios from 'axios'
-import {colors} from './ulti'
+import {colors, type} from './ulti'
+
 export default class Mapv extends Component {
   constructor() {
     super()
@@ -113,32 +109,24 @@ export default class Mapv extends Component {
                 />
               ))
             : null}
-          {/* <Container style={{position: 'absolute'}}>
-            <Content style={{position: 'absolute'}}>
-              <Form style={{position: 'absolute'}}>
-                <Item picker style={{position: 'absolute'}}>
-                  <Picker
-                    mode="dropdown"
-                    iosIcon={<Icon name="arrow-down" />}
-                    style={{position: 'absolute'}}
-                    placeholder="Select your SIM"
-                    placeholderStyle={{color: '#bfc6ea'}}
-                    placeholderIconColor="#007aff"
-                    selectedValue={this.state.selected2}
-                    onValueChange={this.onValueChange2.bind(this)}
-                  >
-                    <Picker.Item label="Wallet" value="key0" />
-                    <Picker.Item label="ATM Card" value="key1" />
-                    <Picker.Item label="Debit Card" value="key2" />
-                    <Picker.Item label="Credit Card" value="key3" />
-                    <Picker.Item label="Net Banking" value="key4" />
-                  </Picker>
-                </Item>
-              </Form>
-            </Content>
-          </Container> */}
         </MapView>
-          <Button><Text> CLICK  ME</Text></Button>
+        <Item picker>
+          <Picker
+            mode="dropdown"
+            iosIcon={<Icon name="arrow-down" />}
+            placeholder="Select your SIM"
+            placeholderStyle={{color: '#bfc6ea'}}
+            placeholderIconColor="#007aff"
+            selectedValue={this.state.selected2}
+            onValueChange={this.onValueChange2.bind(this)}
+          >
+            <Picker.Item label="Wallet" value="key0" />
+            <Picker.Item label="ATM Card" value="key1" />
+            <Picker.Item label="Debit Card" value="key2" />
+            <Picker.Item label="Credit Card" value="key3" />
+            <Picker.Item label="Net Banking" value="key4" />
+          </Picker>
+        </Item>
       </View>
     )
   }
