0c45a1178e35522f78fe4f9ec6ab0dd0fccba651
Avaree <avaree.warrick@gmail.com>
2019-11-26 16:24:14 -0600

    updated client/redux/user - temp

--- client/redux/user.js
+++ client/redux/user.js
@@ -1,93 +1,48 @@
-import axios from 'axios'
-import {AsyncStorage} from 'react-native'
-
-const {url} = require('../../secrets')
-console.log('URL', url)
+import axios from "axios";
 
 //action type
-const GETUSER = 'GETUSER'
-const ADDUSER = 'ADDUSER'
+const GETUSER = "GETUSER";
+const ADDUSER = "ADDUSER";
 
 //action creator
-export const setUser = user => ({type: GETUSER, user})
-export const addUser = user => ({type: ADDUSER, user})
+export const setUser = user => ({ type: GETUSER, user });
+export const addUser = user => ({ type: ADDUSER, user });
 
 //state
-const initialState = {}
+const initialState = {};
 
 //thunk
 export const fetchUserLogin = values => async dispatch => {
   try {
-    const email = values.email
-    const password = values.password
-    let {data} = await axios({
-      url: `${url}/graphql`,
-      method: 'POST',
-      data: {
-        query: `
-        {
-          userLogin(email: "${email}", password: "${password}") {
-              id
-              email
-              fullName
-              homeLocation
-              incentivePoints
-              profilePicture
-            }
-        }
-        `
-      }
-    })
-    dispatch(setUser(data))
+    const email = values.email;
+    const password = values.password;
+    //console.log("Email: ", email, "Password: ", password);
+    //const {data} = await axios.get({ url :'/graphql' , method: 'get', data:{}})
   } catch (error) {
-    alert('COULD NOT LOGIN')
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
-export const userSignUp = (values, preferences) => async dispatch => {
+export const userSignUp = values => async dispatch => {
   try {
-    console.log(values, preferences)
-
-    const fullName = values.fullName
-    const age = values.age
-    const password = values.password
-    const email = values.email
-    const address = values.address
-    const radius = values.radius
-
-    let {data} = await axios({
-      url: `${url}/graphql`,
-      method: 'POST',
-      data: {
-        query: `
-        {
-          userSignup(fullName: "${fullName}", age: "${age}", homeLocation: "${address}", radius: "${radius}", email: "${email}", password: "${password}") {
-            id
-            email
-            fullName
-            homeLocation
-            incentivePoints
-            profilePicture 
-            }
-        }
-        `
-      }
-    })
-    dispatch(setUser(data.data.userSignup))
+    const fullname = values.fullname;
+    const age = values.age;
+    const gender = values.gender;
+    const password = values.password;
+    console.log(values);
+    //const {data} = await axios.get({ url :'/graphql' , method: 'post', data:{}})
   } catch (error) {
-    alert('COULD NOT SIGN-UP')
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case GETUSER:
-      return action.user
+      return action.user;
     case ADDUSER:
-      return action.user
+      return action.user;
     default:
-      return state
+      return state;
   }
-}
+};
